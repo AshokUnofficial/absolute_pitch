faa62aca154243750a129697b401bce61cbae19c
bhavsinh.futurebeedigital <bhavsinh@futurebeedigital.com>
2024-10-16 20:18:15 +0530

    Change ? Button

--- src/MusicWheel.js
+++ src/MusicWheel.js
@@ -29,13 +29,12 @@ import DurationDefault from "../public/assets/images/Duration.png";
 import Duration1 from "../public/assets/images/GroupDuration1.png";
 import Duration2 from "../public/assets/images/GroupDuration2.png";
 import Duration3 from "../public/assets/images/GroupDuration3.png";
-import DataTable from "react-data-table-component";
+// import DataTable from "react-data-table-component";
 import nl2br from "react-nl2br";
-import link from "next/link";
-import LabelIcon from "@mui/icons-material/Label";
-import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-import AutorenewIcon from "@mui/icons-material/Autorenew";
-import RefreshIcon from "@mui/icons-material/Refresh";
+// import LabelIcon from "@mui/icons-material/Label";
+// import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+// import AutorenewIcon from "@mui/icons-material/Autorenew";
+// import RefreshIcon from "@mui/icons-material/Refresh";
 import "./style.module.css";
 
 const customStyles = {
@@ -1386,14 +1385,15 @@ function MusicWheel(props) {
             </Grid>
           </Grid>
         </div>
-        <a href="/Tips" target="_blank" style={{
+
+        <Link href="/Tips" target="_blank" style={{
           textDecoration: 'none', color: 'inherit', marginRight: "16px",
           marginLeft: "-73px"
         }} >
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-question-circle-fill" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" marginLeft="16px" width="32" height="32" fill="currentColor" className="bi bi-question-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
           </svg>
-        </a>
+        </Link>
         <button
           id="admin-pannel"
           style={{
